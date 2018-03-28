66f34750bfac923aadee81423eb2215eb7304cd8
Fillipedem <fillipedem@gmail.com>
2018-03-28 10:13:30 -0300

    Simple Twitter Crawler Plot CSV Graph File

--- main.py
+++ main.py
@@ -1,8 +1,66 @@
-from crawler import TwitterCrawler
+"""
+Crawler
+"""
+import tweepy
+import time
 
 
-crawler = TwitterCrawler("Fillipedem", 2)
-s = crawler.search()
+# Graph
+graph = {}
+frontier = []
+file_graph = 'graphs\search.txt'
+ids_to_screen = {}
 
-for key in s:
-    print(key, len(s[key]))
+# Tweepy API Auth
+print("Autenticado")
+auth = tweepy.OAuthHandler('preencher', 'preencher')
+auth.set_access_token('preencher',
+                        'preencher')
+
+api = tweepy.API(auth, wait_on_rate_limit=True)
+
+# Search Followers from start user
+print("Buscando Followers")
+user = api.get_user('carequinha10')
+
+frontier.append(user.id)
+
+for friends in tweepy.Cursor(api.followers_ids, user_id=user.id).pages():
+
+    frontier.extend(friends)
+    #for f in friends:
+    #    frontier.append(f.screen_name)
+
+print(len(frontier))
+
+# Search Following
+print("Buscando Following")
+for user in frontier:
+    # log
+    print("Get user: " + str(user))
+
+    # adding node
+    graph[user] = []
+
+    try:
+        # search followings on frontier
+        for page in tweepy.Cursor(api.friends_ids, user_id=user).pages(2):
+
+            graph[user].extend(page)
+    except tweepy.error.TweepError:
+        print("Usuário: " + str(user) + " Não Autorizado!!")
+
+    # escrevendo
+    with open(file_graph, 'a') as f:
+
+        f.write(str(user))
+
+        for friend in graph[user]:
+            f.write(";" + str(friend))
+
+        f.write('\n')
+
+    time.sleep(15)
+
+
+
